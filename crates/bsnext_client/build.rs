11923b4eac2870a4bacb9f798ce362d01ee558c8
Shane Osbourne <sosbourne@duckduckgo.com>
2024-05-15 09:12:53 +0100

    skip some stuff in CI

--- crates/bsnext_client/build.rs
+++ crates/bsnext_client/build.rs
@@ -1,5 +1,5 @@
 use std::env::current_dir;
-use std::io;
+use std::{env, io};
 use std::io::Write;
 use std::process::Command;
 
@@ -12,31 +12,33 @@ fn main() {
         .expect("parent 2")
         .to_path_buf();
 
-    let output = Command::new("typeshare")
-        .args([
-            "crates/bsnext_core",
-            "--lang=typescript",
-            "--output-file=crates/bsnext_client/generated/dto.ts",
-        ])
-        .current_dir(&root)
-        .output()
-        .expect("build types");
+    if env::var("CI").is_err() {
+        let output = Command::new("typeshare")
+            .args([
+                "crates/bsnext_core",
+                "--lang=typescript",
+                "--output-file=crates/bsnext_client/generated/dto.ts",
+            ])
+            .current_dir(&root)
+            .output()
+            .expect("build types");
 
-    io::stdout().write_all(&output.stdout).unwrap();
-    io::stderr().write_all(&output.stderr).unwrap();
+        io::stdout().write_all(&output.stdout).unwrap();
+        io::stderr().write_all(&output.stderr).unwrap();
 
-    assert!(output.status.success());
+        assert!(output.status.success());
 
-    let output = Command::new("npm")
-        .args(["run", "schema"])
-        .current_dir(root.join("crates").join("bsnext_client"))
-        .output()
-        .expect("build schema");
+        let output = Command::new("npm")
+            .args(["run", "schema"])
+            .current_dir(root.join("crates").join("bsnext_client"))
+            .output()
+            .expect("build schema");
 
-    io::stdout().write_all(&output.stdout).unwrap();
-    io::stderr().write_all(&output.stderr).unwrap();
+        io::stdout().write_all(&output.stdout).unwrap();
+        io::stderr().write_all(&output.stderr).unwrap();
 
-    assert!(output.status.success());
+        assert!(output.status.success());
+    }
 
     let output = Command::new("npm")
         .args(["run", "build"])
