6b0786a57055ec4457f6bfb5cb17d5091b9102d8
Shane Osbourne <sosbourne@duckduckgo.com>
2024-08-17 21:38:41 +0100

    Added an example for delay

--- tests/examples.spec.ts
+++ tests/examples.spec.ts
@@ -1,7 +1,7 @@
 import {bstest, test} from './utils';
 import {expect} from "@playwright/test";
 
-test.describe('examples/basic', {
+test.describe('examples/basic/headers.yml', {
   annotation: {
     type: bstest({
       input: 'examples/basic/headers.yml'
@@ -26,5 +26,26 @@ test.describe('examples/basic', {
       abc: 'def',
     }
     expect(headers).toMatchObject(expected)
+  });
+})
+
+test.describe('examples/basic/delays.yml', {
+  annotation: {
+    type: bstest({
+      input: 'examples/basic/delays.yml'
+    }),
+    description: ''
+  }
+}, () => {
+  test('first delay item', async ({request, bs}) => {
+    const start = Date.now();
+    const response = await request.get(bs.path('/'));
+    
+    const body = await response.body();
+    const diff = Date.now() - start;
+
+    expect(body.toString()).toBe(`first - 200ms delay`)
+    expect(diff).toBeGreaterThan(200)
+    expect(diff).toBeLessThan(300)
   });
 })
