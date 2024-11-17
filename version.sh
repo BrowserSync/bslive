a1d57d5d090de93c8047aef36680d49b52693c5b
Shane Osbourne <sosbourne@duckduckgo.com>
2024-11-17 12:12:39 +0000

    Set version to 0.2.0

--- version.sh
+++ version.sh
@@ -1,9 +1,9 @@
 set -euxo pipefail
 
-if ! git diff-index --quiet HEAD --; then
-    echo "Cannot version, there are changes in the working tree."
-    exit 1
-fi
+#if ! git diff-index --quiet HEAD --; then
+#    echo "Cannot version, there are changes in the working tree."
+#    exit 1
+#fi
 
 
 if [ -z "$1" ]; then
@@ -15,3 +15,15 @@ npm version "$1" --no-git-tag-version --workspaces --include-workspace-root
 cargo set-version "$1"
 
 echo "did set version to $1"
+
+
+version=$(jq -r '.version' package.json)
+echo "Current version in package.json is $version"
+
+vversion="v$version"
+echo "Version with 'v' prepended is $vversion"
+
+git add .
+git commit -m "Set version to $version"
+git tag "$vversion"
+git push origin main --tags
