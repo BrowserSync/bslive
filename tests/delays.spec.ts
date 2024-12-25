0f35eacd69a4371e60535b9099e92a484676f523
Shane Osbourne <shane.osbourne8@gmail.com>
2024-12-25 19:42:15 +0000

    added playwright test

--- tests/delays.spec.ts
+++ tests/delays.spec.ts
@@ -23,6 +23,19 @@ test.describe(
             expect(diff).toBeGreaterThan(200);
             expect(diff).toBeLessThan(300);
         });
+        test("no config-based delay (url param)", async ({ request, bs }) => {
+            const start = Date.now();
+            const response = await request.get(
+                bs.path("/none?bslive.delay.ms=200"),
+            );
+
+            const body = await response.body();
+            const diff = Date.now() - start;
+
+            expect(body.toString()).toBe(`no config-based delay`);
+            expect(diff).toBeGreaterThan(200);
+            expect(diff).toBeLessThan(300);
+        });
         test("500ms delay", async ({ request, bs }) => {
             const start = Date.now();
             const response = await request.get(bs.path("/500"));
