9756ded32db52f29f38686a753289df232be6c23
Shane Osbourne <shane.osbourne8@gmail.com>
2025-06-22 13:18:10 +0100

    support initial run

--- crates/bsnext_system/src/watch/mod.rs
+++ crates/bsnext_system/src/watch/mod.rs
@@ -11,8 +11,8 @@ pub struct WatchCommand {
     #[arg(long = "command", short)]
     pub command: Vec<String>,
     /// Initial command to run before starting watchers
-    #[arg(long = "initial", short)]
-    pub initial: Vec<String>,
+    #[arg(long = "initial", short, default_value = "false")]
+    pub initial: bool,
     /// provide this flag to disable command prefixes
     #[arg(long = "no-prefix", default_value = "false")]
     pub no_prefix: bool,
@@ -37,24 +37,27 @@ impl From<WatchCommand> for MultiWatch {
             })
             .collect::<Vec<_>>();
 
-        let before = value
-            .initial
-            .iter()
-            .map(ToOwned::to_owned)
-            .enumerate()
-            .map(move |(index, item)| {
-                let name = Some(format!("initial:{}", index));
-                let prefix = value.no_prefix.then_some(PrefixOpt::Bool(false));
-                BeforeRunOptItem::Sh(ShRunOptItem {
-                    sh: item,
-                    name,
-                    prefix,
+        let before = value.initial.then(|| {
+            let cloned = run_opts.clone();
+            cloned
+                .into_iter()
+                .map(|old| match old {
+                    RunOptItem::Sh(old) => {
+                        let name = Some("initial".to_string());
+                        let prefix = value.no_prefix.then_some(PrefixOpt::Bool(false));
+                        BeforeRunOptItem::Sh(ShRunOptItem {
+                            sh: old.sh,
+                            name,
+                            prefix,
+                        })
+                    }
+                    _ => unreachable!(),
                 })
-            })
-            .collect::<Vec<_>>();
+                .collect::<Vec<_>>()
+        });
 
         let spec = Spec {
-            before: Some(before),
+            before,
             run: Some(run_opts),
             ..Default::default()
         };
