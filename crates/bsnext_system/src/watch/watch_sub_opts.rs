aaa7bdb142affafa8c7e481bf96f52a9cf1ce97e
Shane Osbourne <shane.osbourne8@gmail.com>
2026-04-10 22:17:16 +0100

    ssing files

---
+++ crates/bsnext_system/src/watch/watch_sub_opts.rs
@@ -0,0 +1,71 @@
+use crate::watch::watch_runner::WatchRunnerStr;
+use bsnext_input::route::{
+    BeforeRunOptItem, MultiWatch, RunOptItem, ShRunOptItem, Spec, WatcherDirs,
+};
+
+#[derive(Debug, Default, Clone, clap::Parser)]
+pub struct WatchSubOpts {
+    #[arg(long = "watch.path", num_args(0..))]
+    pub paths: Vec<String>,
+    #[arg(long = "watch.run", num_args(0..))]
+    pub run: Vec<WatchRunnerStr>,
+    #[arg(long = "watch.before", num_args(0..))]
+    pub before: Vec<WatchRunnerStr>,
+    #[arg(long = "watch.initial")]
+    pub initial: bool,
+}
+
+impl From<WatchSubOpts> for MultiWatch {
+    fn from(value: WatchSubOpts) -> Self {
+        let dirs = WatcherDirs::Many(value.paths);
+        let run_opts = value
+            .run
+            .iter()
+            .map(ToOwned::to_owned)
+            .map(move |item| match item {
+                WatchRunnerStr::Sh(sh) => RunOptItem::Sh(ShRunOptItem {
+                    sh,
+                    name: None,
+                    prefix: None,
+                }),
+                WatchRunnerStr::BsLive(bslive) => RunOptItem::BsLive { bslive },
+            })
+            .collect::<Vec<_>>();
+
+        let initial = value.initial.then(|| {
+            run_opts
+                .iter()
+                .map(ToOwned::to_owned)
+                .filter_map(move |item| match item {
+                    RunOptItem::Sh(sh) => Some(BeforeRunOptItem::Sh(sh)),
+                    _ => None,
+                })
+                .collect::<Vec<_>>()
+        });
+
+        let before_explicit = (!value.before.is_empty()).then(|| {
+            value
+                .before
+                .iter()
+                .filter_map(|watch| match watch {
+                    WatchRunnerStr::Sh(sh) => Some(BeforeRunOptItem::Sh(ShRunOptItem {
+                        sh: sh.to_owned(),
+                        name: None,
+                        prefix: None,
+                    })),
+                    _ => None,
+                })
+                .collect::<Vec<_>>()
+        });
+
+        let spec = Spec {
+            before: initial.or(before_explicit),
+            run: Some(run_opts),
+            ..Default::default()
+        };
+        MultiWatch {
+            dirs,
+            opts: Some(spec),
+        }
+    }
+}
